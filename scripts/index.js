c6d10dd6a5de545f020f4826632d97e1f10b70f5
Sofya Kreys <sofya.kreis@yandex.ru>
2024-09-30 16:57:32 +0700

    Update js

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function deleteCard(e) {
+    let card = e.target.closest('.card');
+    card.remove();
+}
+
+function Card() {
+    const cardTemplate = document.querySelector('#card-template').content;
+    initialCards.forEach((item) => {
+        const cardTemplateCopy = cardTemplate.cloneNode(true);
+        cardTemplateCopy.querySelector('.card__image').setAttribute("src", item.link);
+        cardTemplateCopy.querySelector('.card__title').textContent = item.name;
+
+        const deleteButton = cardTemplateCopy.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', deleteCard);
+
+        const list = document.querySelector('.places__list'); 
+        list.append(cardTemplateCopy);
+    })
+} 
+
+Card();
